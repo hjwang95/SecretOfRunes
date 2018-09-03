2ed37260c023c34edfe9a14e4aeac3344f127006
Huijun Wang <jevelyn@rams.colostate.edu>
2018-09-03 11:00:15 -0700

    moving platforms&coins(not done)

---
+++ Assets/Scripts/eatCoin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eatCoin : MonoBehaviour {
+
+    // Use this for initialization
+    public int pointsToadd;
+    void startTrigger (Collider2D c){
+        if (c.GetComponent<PlayerController>() == null)
+            return;
+
+        //add coins to the scoreboard in the future here
+
+        Destroy(gameObject);
+
+
+    }
+
+}
---
+++ Assets/Scripts/rockAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rockAction : MonoBehaviour
+{
+    public GameObject platform;
+    public float speed;
+    public Transform cpoint;
+
+    public Transform[] points;
+    public int pointChoice;
+
+	 void Start()
+	{
+        cpoint = points[pointChoice];
+	}
+
+	void Update()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, cpoint.position, Time.deltaTime * speed);
+
+        //choose position to be back
+        if(platform.transform.position == cpoint.position){
+            //go through each point
+            pointChoice ++;
+            //check whether it is the end point
+            if(pointChoice == points.Length){
+                pointChoice = 0;
+            }
+            //reset after every frame to move on
+            cpoint = points[pointChoice];
+        }
+    }
+}
